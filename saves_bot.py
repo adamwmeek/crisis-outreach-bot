42c601caca2917b024d0d483d452c01d014c08e6
genraiyama <gen.raiyama@gmail.com>
2021-03-14 22:14:26 -0500

    Initial commit of existing files

---
+++ saves_bot.py
@@ -0,0 +1,100 @@
+import os
+import logging as saves_logger
+
+import discord
+from discord.ext import commands
+
+KEYWORDS_FILENAME = 'keywords.txt'
+RESPONSE_FILENAME = 'response.txt'
+VOLUNTEER_CHANNEL_ID = 820813652705673227
+VOLUNTEER_MESSAGE = 'I picked up on a keyword in conversation: '
+
+from dotenv import load_dotenv
+load_dotenv()
+# TODO: Remember to remove me!
+TOKEN = os.getenv('BOT_TOKEN')
+
+saves_logger.basicConfig(level=saves_logger.DEBUG, format='%(asctime)s %(message)s',
+                    handlers=[saves_logger.FileHandler("saves_bot.log"), saves_logger.StreamHandler()])
+bot = commands.Bot(command_prefix='$')
+KEYWORDS = []
+RESPONSE_MESSAGE = ''
+client = discord.Client()
+
+def _reload_response():
+    with open(RESPONSE_FILENAME, 'r') as filehandle:
+        RESPONSE_MESSAGE = ''
+        contents = filehandle.readlines()
+
+        RESPONSE_MESSAGE = ''.join(map(str, contents))
+        saves_logger.debug(f'Loaded response: {RESPONSE_MESSAGE}')
+        return RESPONSE_MESSAGE
+
+RESPONSE_MESSAGE = _reload_response()
+
+def _reload_keywords():
+    KEYWORDS.clear()
+    with open(KEYWORDS_FILENAME, 'r') as filehandle:
+        contents = filehandle.readlines()
+
+        for line in contents:
+            line = line.strip('\r')
+            line = line.strip('\n')
+            KEYWORDS.append(line.lower())
+            saves_logger.debug(f'Added keyword: {line}')
+            
+_reload_keywords()
+
+def _add_keyword(keyword):
+    KEYWORDS.append(keyword.lower())
+
+    with open(KEYWORDS_FILENAME, 'w') as filehandle:
+            filehandle.writelines(KEYWORDS)
+
+def _remove_keyword(keyword):
+    KEYWORDS.remove(keyword.lower())
+
+    with open(KEYWORDS_FILENAME, 'w') as filehandle:
+            filehandle.writelines(KEYWORDS)
+
+@client.event
+async def on_message(message):
+    # Ignore messages in volunteer channel
+    if message.channel.id == VOLUNTEER_CHANNEL_ID:
+        return
+
+    # Ignore messages from bots
+    if message.author.bot:
+        return
+
+    # If we see a keyword in any message, take the following actions:
+    if any([keyword in message.content for keyword in KEYWORDS]):
+        saves_logger.debug(f'Picked up on keyword in channel {message.channel.name} in message: {message.content}')
+
+        # Send crisis info to author of message
+        await message.author.send(RESPONSE_MESSAGE)
+
+        # Put a reply in that channel
+        # await message.channel.send(RESPONSE_MESSAGE)
+
+        # Let the volunteer channel know of the incident
+        volunteer_channel = client.get_channel(VOLUNTEER_CHANNEL_ID)
+        await volunteer_channel.send(VOLUNTEER_MESSAGE + message.content)
+
+@bot.command(name='add')
+async def add_keyword(context, keyword):
+    # Look for commands from admins
+    await context.send('test')
+    if any([role.name == 'Admin' for role in context.author.roles]):
+        _add_keyword(keyword)
+        await context.send(f'Adding keyword: {keyword}')
+
+@bot.command(name='remove')
+async def remove_keyword(context, keyword):
+    # Look for commands from admins
+    await context.send('test')
+    if any([role.name == 'Admin' for role in context.author.roles]):
+        _remove_keyword(keyword)
+        await context.send(f'Removing keyword: {keyword}')
+
+client.run(TOKEN)
